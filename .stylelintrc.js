b9bd8b4a074f2fc33c29f1cd096fb9a6bc8ca87a
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2026-04-23 00:18:12 -0500

    chore: add stylelint css properties order

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,11 +1,74 @@
 export default {
   extends: [
-    'stylelint-config-standard-scss',
+    "stylelint-config-standard-scss"
   ],
   plugins: [
-    'stylelint-order',
+    "stylelint-order"
   ],
   rules: {
+    "order/properties-order": [
+      [
+        {
+          groupName: "layout",
+          properties: [
+            "display",
+            "position",
+            "top",
+            "right",
+            "bottom",
+            "left",
+            "z-index"
+          ]
+        },
+        {
+          groupName: "box-model",
+          properties: [
+            "width",
+            "min-width",
+            "max-width",
+            "height",
+            "margin",
+            "padding"
+          ]
+        },
+        {
+          groupName: "flex",
+          properties: [
+            "flex",
+            "flex-direction",
+            "justify-content",
+            "align-items",
+            "gap"
+          ]
+        },
+        {
+          groupName: "visual",
+          properties: [
+            "background",
+            "background-color",
+            "border",
+            "border-radius"
+          ]
+        },
+        {
+          groupName: "typography",
+          properties: [
+            "font-size",
+            "font-weight",
+            "text-align",
+            "color"
+          ]
+        },
+        {
+          groupName: "interaction",
+          properties: [
+            "cursor",
+            "transition",
+            "transform"
+          ]
+        }
+      ]
+]
   },
   ignoreFiles: ['src/scss/vendors/_normalize.scss'],
 };
